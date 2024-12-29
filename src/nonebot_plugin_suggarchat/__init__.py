8291f952ce3ef5acdb06f000d432944a697da1f2
JohnRichard4096 <windowserror@163.com>
2024-12-29 21:59:25 +0800

    修复无法加载的异常

--- src/nonebot_plugin_suggarchat/__init__.py
+++ src/nonebot_plugin_suggarchat/__init__.py
@@ -11,11 +11,10 @@ from .API import *
 __plugin_meta__ = PluginMetadata(
     name="nonebot_plugin_suggarchat",
     description="Plugin for the Suggar chat framework compatible with Nonebot2.",
-    usage="use OpenAI API to chat",
+    usage="按照Readme.md修改配置文件后使用。",
     config=Config,
     homepage="https://github.com/JohnRichard4096/nonebot_plugin_suggarchat/",
     type="application",
-    author="JohnRichard4096",
     version=__KERNEL_VERSION__,
     supported_adapters={"~onebot.v11"}
 )
