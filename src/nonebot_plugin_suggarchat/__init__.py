905a1b0a523c97294d098079c7d16dbdbad935ed
JohnRichard4096 <windowserror@163.com>
2025-01-01 08:55:07 +0800

    change info

--- src/nonebot_plugin_suggarchat/__init__.py
+++ src/nonebot_plugin_suggarchat/__init__.py
@@ -9,9 +9,9 @@ from .suggar import *
 from .API import *
 
 __plugin_meta__ = PluginMetadata(
-    name="nonebot_plugin_suggarchat",
-    description="Plugin for the Suggar chat framework compatible with Nonebot2.",
-    usage="按照Readme.md修改配置文件后使用。",
+    name="SuggarChat OpenAI协议聊天插件" ,
+    description="强大的插件，支持OpenAI协议，多模型切换，完全的上下文支持，智能化的聊天。适配Nonebot2-Onebot-V11适配器",
+    usage="按照Readme.md修改配置文件后使用，默认enable为false！",
     config=Config,
     homepage="https://github.com/JohnRichard4096/nonebot_plugin_suggarchat/",
     type="application",
