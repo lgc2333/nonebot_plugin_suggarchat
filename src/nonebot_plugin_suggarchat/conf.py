e348080efc06f8889edcc8cccfe7263d4063606e
JohnRichard4096 <windowserror@163.com>
2025-02-17 20:52:22 +0800

    Change version

--- src/nonebot_plugin_suggarchat/conf.py
+++ src/nonebot_plugin_suggarchat/conf.py
@@ -1,7 +1,7 @@
 import os
 from nonebot.adapters.onebot.v11 import Bot
 import nonebot_plugin_localstore as store
-__KERNEL_VERSION__:str = "V1.10.1-Public"
+__KERNEL_VERSION__:str = "V1.10.2-Public"
 # 获取当前工作目录  
 current_directory:str = os.getcwd()  
 config_dir = store.get_plugin_config_dir()
