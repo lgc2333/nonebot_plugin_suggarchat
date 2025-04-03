5b4dbe6cc33bf86027c31043dc7395a7d5e64235
JohnRichard4096 <windowserror@163.com>
2025-04-03 21:08:08 +0800

    version

--- src/nonebot_plugin_suggarchat/config.py
+++ src/nonebot_plugin_suggarchat/config.py
@@ -8,7 +8,7 @@ import tomli
 import tomli_w
 from pydantic import BaseModel
 
-__KERNEL_VERSION__: str = "V2.0.3.post.2-Public"
+__KERNEL_VERSION__: str = "V2.0.3.1.post2-Public"
 
 # 配置目录
 CONFIG_DIR: Path = store.get_plugin_config_dir()
